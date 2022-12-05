23d5911bb3754f573b1ff2a0fbaa3ee6172195b7
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-05 17:20:57 +0530

    changes

---
+++ event.js
@@ -0,0 +1,5 @@
+
+document.getElement(“ninjas”).innerHTML=”I am a Ninja”;
+document.getElementById(“ninjas”).innerHTML=I am a Ninja;
+document.getElementById(“ninjas”).innerHTML=”I am a Ninja”;
+document.querySelector(“#ninjas”).innerHTML= “I am a Ninja”
