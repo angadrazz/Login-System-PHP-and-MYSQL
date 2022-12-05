6f9e0f0716a5b066e46b0ea8108f9094efa2ff78
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-05 17:22:18 +0530

    removed

--- event.js
+++
@@ -1,5 +0,0 @@
-
-document.getElement(“ninjas”).innerHTML=”I am a Ninja”;
-document.getElementById(“ninjas”).innerHTML=I am a Ninja;
-document.getElementById(“ninjas”).innerHTML=”I am a Ninja”;
-document.querySelector(“#ninjas”).innerHTML= “I am a Ninja”
